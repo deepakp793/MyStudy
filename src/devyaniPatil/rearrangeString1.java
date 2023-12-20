e9f92851c80615e093d82e94e2a9b768b34cd72c
Devyani <devyanipatil997@gmail.com>
2023-12-20 23:50:19 +0530

    Assignment_18

---
+++ src/devyaniPatil/rearrangeString1.java
@@ -0,0 +1,35 @@
+package devyaniPatil;
+/*Assignment - 18 :
+2) Write a program to rearrange String in given format. 
+Hint : Uppercase letters + digits + LowerCase letter
+input = "t8EcHNo5Cred3iTs";
+output : EHNCT853tcoredis 
+// Character.isUpperCase(ch), Character.isLowerCase(ch)
+*/
+
+class rearrangeString1 {
+
+	String AllDigitAllCharacter(String input) {
+		String tempUppercaseLetter = "";
+		String tempDigits = "";
+		String tempLowerCaseLetter = "";
+
+		for (int index = 0; index < input.length(); index++) {
+			char ch = input.charAt(index);
+			if (Character.isUpperCase(ch)) {
+				tempUppercaseLetter = tempUppercaseLetter + ch;
+			} else if (Character.isDigit(ch)) {
+				tempDigits = tempDigits + ch;
+			} else if (Character.isLowerCase(ch)) {
+				tempLowerCaseLetter = tempLowerCaseLetter + ch;
+			}
+		}
+		return tempUppercaseLetter + tempDigits + tempLowerCaseLetter;
+	}
+
+	public static void main(String[] args) {
+		rearrangeString1 r = new rearrangeString1();
+		String output = r.AllDigitAllCharacter("t8EcHNo5Cred3iTs");
+		System.out.println(output);
+	}
+}
