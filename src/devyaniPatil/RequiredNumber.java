e57bdec8f85278f61389d6ffa486f4c4746e22ac
Devyani <devyanipatil997@gmail.com>
2023-12-20 14:14:09 +0530

    Resolved conflicts, including file deletion.

---
+++ src/devyaniPatil/RequiredNumber.java
@@ -0,0 +1,25 @@
+package devyaniPatil;
+/*test:2
+findRequiredNumbers(50);
+input : 50
+output : 10*/
+
+class RequiredNumber{
+
+ void findRequiredNumber(int targetNum){
+	int no=1;
+	int count=0;
+	int sum=0;
+   while(sum<targetNum){
+			count++;
+			sum=sum+no;
+			no++;
+   } 
+   System.out.println(count);
+   
+ }
+ public static void main(String[]args){
+   RequiredNumber N=new RequiredNumber();
+   N.findRequiredNumber(50);
+ }
+}
