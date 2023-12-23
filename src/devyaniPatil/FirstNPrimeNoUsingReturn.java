d196676c8092fc4ac36747b6df148256d234fd25
Devyani <devyanipatil997@gmail.com>
2023-12-23 12:29:04 +0530

    RemovedSpacing

--- src/devyaniPatil/FirstNPrimeNoUsingReturn.java
+++ src/devyaniPatil/FirstNPrimeNoUsingReturn.java
@@ -8,33 +8,32 @@ output: (2 + 3 + 5 + 7 + 11) = 28
 
 class FirstNPrimeNoUsingReturn {
 
-    boolean isPrimeNum(int num) {
-        boolean isPrimeFlag = true;
-        for (int index = 2; index <= num / 2; index++) {
-            if (num % index == 0) {
-                isPrimeFlag = false;
-                break;
-            }
-        }
-        return isPrimeFlag;
-    }
-	
-      int sumOfFirstNPrimeNumber(int n) {
-        int count = 0;
-        int num = 2;
-        int sum = 0;
-        while (count < n) {
-            if (isPrimeNum(num)) {
-                sum = sum + num;
-                count++;
-            }
-            num++;
-        }
-        return sum;
-    }
-    public static void main(String[] args) {
-        FirstNPrimeNoUsingReturn F = new FirstNPrimeNoUsingReturn();
-        int n = F.sumOfFirstNPrimeNumber(5);
-        System.out.println("Sum of first 5 prime numbers: " + n);
-    }
+	boolean isPrimeNum(int num) {
+		boolean isPrimeFlag = true;
+		for (int index = 2; index <= num / 2; index++) {
+			if (num % index == 0) {
+				isPrimeFlag = false;
+				break;
+			}
+		}
+		return isPrimeFlag;
+	}
+	int sumOfFirstNPrimeNumber(int n) {
+		int count = 0;
+		int num = 2;
+		int sum = 0;
+		while (count < n) {
+			if (isPrimeNum(num)) {
+				sum = sum + num;
+				count++;
+			}
+			num++;
+		}
+		return sum;
+	}
+	public static void main(String[] args) {
+		FirstNPrimeNoUsingReturn F = new FirstNPrimeNoUsingReturn();
+		int n = F.sumOfFirstNPrimeNumber(5);
+		System.out.println("Sum of first 5 prime numbers: " + n);
+	}
 }
