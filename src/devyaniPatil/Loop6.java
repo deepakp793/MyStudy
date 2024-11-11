93c88248d853b7c0a17a8797ed55c5ddde3d6931
Devyani <devyanipatil997@gmail.com>
2024-11-11 14:45:18 +0530

    Remove unwanted variable

--- src/devyaniPatil/Loop6.java
+++ src/devyaniPatil/Loop6.java
@@ -1,7 +1,7 @@
 package devyaniPatil;
 class Loop6{
 	void m1(){
-		int count = 0;
+		//int count = 0;
 		 for(int i=1;i<=100;i++){ 
 		 if(i%7==0 && i%13==0){
 		 System.out.println(i+ " divisible by 7 and 13");
--- src/devyaniPatil/Loopex3.java
+++ src/devyaniPatil/Loopex3.java
@@ -1,19 +1,25 @@
 package devyaniPatil;
 class Loopex3{
 	void processData(int startno,int endno){
-		
+		int count = 0;
 		for( int num = startno; num<=endno;num++){
-			int count = 0;
 			if(num%3 == 0 && num%7 == 0){
 				count++;
-				
+				System.out.println(num);
+				if(count==3) {
+					break;
+				}
 			}
-			System.out.println(num);
+			
 		}
+		System.out.println(count);
 		
 	}
+	
+	
+	
  	public static void main(String[]args){
 		Loopex3 loopex3 = new Loopex3();
-		loopex3.processData(1,100);
+		loopex3.processData(20,100);
 	}
 }
